66f86380813ccdb1d2da272a910d89de099cfd17
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-17 14:31:21 +0100

    prima richiesta

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void printArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+
+}
+
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
+
+
+int[] array = {2,6,7,5,3,9};
+
+//stampare l'array di numeri fornito a video
+printArray(array);  
